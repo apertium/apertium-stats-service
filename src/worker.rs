93f43e921c37dbb99d9f03f6882564e152063ab2
Sushain Cherivirala <sushain@skc.name>
2018-03-11 01:50:07 -0600

    Get revision from SVN XML list repos

--- src/worker.rs
+++ src/worker.rs
@@ -20,6 +20,7 @@ use self::tokio_core::reactor::Core;
 use self::hyper_tls::HttpsConnector;
 use self::quick_xml::reader::Reader;
 use self::quick_xml::events::Event;
+use self::quick_xml::events::attributes::Attribute;
 use self::diesel::prelude::*;
 
 use super::models::NewEntry;
@@ -55,6 +56,65 @@ fn get_file_kind(file_name: &str) -> Option<&str> {
     } else {
         // TODO: implement the rest
         None
+    }
+}
+
+fn list_files(name: &str) -> Result<Vec<(String, i32)>, String> {
+    let output = Command::new("svn")
+        .arg("list")
+        .arg("--xml")
+        .arg("--recursive")
+        .arg(format!("{}/{}/trunk", super::ORGANIZATION_ROOT, name))
+        .output();
+
+    match output {
+        Ok(Output {
+            status, ref stdout, ..
+        }) if status.success() =>
+        {
+            let xml = String::from_utf8_lossy(&stdout);
+            let mut reader = Reader::from_str(&xml);
+            let mut files = Vec::new();
+            let mut in_name = false;
+            let mut name = String::new();
+
+            let mut buf = Vec::new();
+
+            loop {
+                match reader.read_event(&mut buf) {
+                    Ok(Event::Start(ref e)) if e.name() == b"name" => in_name = true,
+                    Ok(Event::Start(ref e)) if e.name() == b"commit" => for a in e.attributes() {
+                        let Attribute { value, key } = a.unwrap();
+                        if str::from_utf8(key).unwrap() == "revision" {
+                            files.push((
+                                name.clone(),
+                                str::from_utf8(&value)
+                                    .unwrap()
+                                    .parse::<i32>()
+                                    .unwrap()
+                                    .clone(),
+                            ));
+                            break;
+                        }
+                    },
+                    Ok(Event::Text(ref e)) if in_name => {
+                        name = e.unescape_and_decode(&reader).unwrap()
+                    }
+                    Ok(Event::End(ref e)) if e.name() == b"name" => in_name = false,
+                    Ok(Event::Eof) => break,
+                    Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+                    _ => (),
+                }
+                buf.clear();
+            }
+
+            Ok(files)
+        }
+        Ok(Output { stderr, .. }) => {
+            let error = String::from_utf8_lossy(&stderr);
+            Err(format!("Package not found: {}", error))
+        }
+        Err(_) => Err(format!("Package search failed: {}", name)),
     }
 }
 
@@ -134,78 +194,63 @@ impl Worker {
         name: &str,
         maybe_kind: Option<&str>,
     ) -> Result<(Tasks, Tasks), String> {
-        let output = Command::new("svn")
-            .arg("list")
-            .arg("-R")
-            .arg(format!("{}/{}/trunk", super::ORGANIZATION_ROOT, name))
-            .output();
+        list_files(name).and_then(|files| {
+            let mut current_tasks = self.current_tasks.lock().unwrap();
+            let current_package_tasks = current_tasks.entry(name.to_string());
 
-        match output {
-            Ok(Output {
-                status, ref stdout, ..
-            }) if status.success() =>
-            {
-                let mut current_tasks = self.current_tasks.lock().unwrap();
-                let current_package_tasks = current_tasks.entry(name.to_string());
-                let new_tasks = String::from_utf8_lossy(&stdout) // TODO: get revision from here and use --xml
-                    .split("\n")
-                    .filter_map(|file| {
-                        get_file_kind(file).and_then(|file_kind| {
-                            let requested_kind = maybe_kind.map_or(true, |kind| kind == file_kind);
-                            let in_progress = match current_package_tasks {
-                                Entry::Occupied(ref occupied) => occupied
-                                    .get()
-                                    .iter()
-                                    .find(
-                                        |&&Task {
-                                             ref kind, ref path, ..
-                                         }| {
-                                            kind == file_kind && path == file
-                                        },
-                                    )
-                                    .is_some(),
-                                _ => false,
-                            };
-                            if requested_kind && !in_progress {
-                                Some(Task {
-                                    kind: file_kind.to_string(),
-                                    path: file.to_string(),
-                                    created: Utc::now().naive_utc(),
-                                    revision: 0, // TODO: get the correct one
-                                })
-                            } else {
-                                None
-                            }
-                        })
-                    })
-                    .collect::<Vec<_>>();
-
-                for task in &new_tasks {
-                    self.launch_task(name, task);
-                }
-
-                match current_package_tasks {
-                    Entry::Occupied(mut occupied) => {
-                        if !new_tasks.is_empty() {
-                            occupied.get_mut().extend(new_tasks.clone());
-                        }
-                        Ok((new_tasks, occupied.get().to_vec()))
-                    }
-                    Entry::Vacant(vacant) => {
-                        if new_tasks.is_empty() {
-                            Ok((new_tasks, Vec::new()))
+            let new_tasks = files
+                .iter()
+                .filter_map(|&(ref file, revision)| {
+                    get_file_kind(file).and_then(|file_kind| {
+                        let requested_kind = maybe_kind.map_or(true, |kind| kind == file_kind);
+                        let in_progress = match current_package_tasks {
+                            Entry::Occupied(ref occupied) => occupied
+                                .get()
+                                .iter()
+                                .find(
+                                    |&&Task {
+                                         ref kind, ref path, ..
+                                     }| {
+                                        kind == file_kind && path == file
+                                    },
+                                )
+                                .is_some(),
+                            _ => false,
+                        };
+                        if requested_kind && !in_progress {
+                            Some(Task {
+                                kind: file_kind.to_string(),
+                                path: file.to_string(),
+                                created: Utc::now().naive_utc(),
+                                revision,
+                            })
                         } else {
-                            Ok((new_tasks.clone(), vacant.insert(new_tasks).clone()))
+                            None
                         }
+                    })
+                })
+                .collect::<Vec<_>>();
+
+            for task in &new_tasks {
+                self.launch_task(name, task);
+            }
+
+            match current_package_tasks {
+                Entry::Occupied(mut occupied) => {
+                    if !new_tasks.is_empty() {
+                        occupied.get_mut().extend(new_tasks.clone());
+                    }
+                    Ok((new_tasks, occupied.get().to_vec()))
+                }
+                Entry::Vacant(vacant) => {
+                    if new_tasks.is_empty() {
+                        Ok((new_tasks, Vec::new()))
+                    } else {
+                        Ok((new_tasks.clone(), vacant.insert(new_tasks).clone()))
                     }
                 }
             }
-            Ok(Output { stderr, .. }) => {
-                let error = String::from_utf8_lossy(&stderr);
-                Err(format!("Package not found: {}", error))
-            }
-            Err(_) => Err(format!("Package search failed: {}", name)),
-        }
+        })
     }
 
     pub fn get_tasks_in_progress(&self, name: &str) -> Option<Tasks> {
