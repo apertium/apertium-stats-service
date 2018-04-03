316efe21331cea1ce2e1a7bf44673f19e1944000
Sushain Cherivirala <sushain@skc.name>
2018-04-03 09:58:40 -0500

    Fail soft lexc parsing

--- src/stats/lexc.rs
+++ src/stats/lexc.rs
@@ -24,6 +24,82 @@ fn get_all_lexicons(lexicons: &Lexicons, root_lexicon: &str) -> BTreeSet<String>
     frontier
 }
 
+fn make_parse_error(line_number: usize, error: &str) -> StatsError {
+    StatsError::Lexc(format!("Unable to parse L{}: {}", line_number, error))
+}
+
+fn parse_line(
+    line: &str,
+    line_number: usize,
+    current_lexicon: &str,
+    lexicons: &mut Lexicons,
+) -> Result<(), StatsError> {
+    let token_count = line.split_whitespace().count();
+
+    if token_count >= 2 {
+        if line.contains(':') {
+            // TODO: me
+        } else {
+            let mut split = line.split(';')
+                .next()
+                .ok_or_else(|| make_parse_error(line_number, "failed to split at ;"))?
+                .trim()
+                .split_whitespace();
+            let lemma = split
+                .next()
+                .ok_or_else(|| make_parse_error(line_number, "failed to get lemma"))?;
+            let continuation_lexicon = split
+                .next()
+                .ok_or_else(|| make_parse_error(line_number, "failed to get continuation lexicon"))?
+                .trim()
+                .split('-')
+                .map(|x| x.to_string())
+                .collect::<BTreeSet<_>>();
+            // let gloss = if line.contains('!') {
+            //     Some(line.split('!').nth(1))
+            // } else {
+            //     None
+            // };
+
+            match lexicons.entry(current_lexicon.to_string()) {
+                Entry::Occupied(mut occupied) => {
+                    occupied
+                        .get_mut()
+                        .1
+                        .insert((lemma.to_string(), continuation_lexicon));
+                }
+                Entry::Vacant(vacant) => {
+                    vacant.insert((
+                        vec![],
+                        HashSet::from_iter(vec![(lemma.to_string(), continuation_lexicon)]),
+                    ));
+                }
+            };
+        }
+    } else if token_count == 1 {
+        let lexicon_pointer = line.split(';')
+            .next()
+            .ok_or_else(|| make_parse_error(line_number, "failed to get lexicon pointer"))?
+            .trim();
+        if lexicon_pointer.contains(' ') {
+            return Err(make_parse_error(line_number, "lexicon pointer has space"));
+        } else {
+            match lexicons.entry(current_lexicon.to_string()) {
+                Entry::Occupied(mut occupied) => {
+                    occupied.get_mut().0.push(lexicon_pointer.to_string());
+                }
+                Entry::Vacant(vacant) => {
+                    vacant.insert((vec![lexicon_pointer.to_string()], HashSet::new()));
+                }
+            };
+        }
+    } else {
+        return Err(make_parse_error(line_number, "missing tokens"));
+    }
+
+    Ok(())
+}
+
 pub fn get_stats(body: hyper::Chunk) -> Result<Vec<(StatKind, String)>, StatsError> {
     let mut current_lexicon: Option<String> = None;
     let mut lexicons: Lexicons = HashMap::new();
@@ -34,7 +110,11 @@ pub fn get_stats(body: hyper::Chunk) -> Result<Vec<(StatKind, String)>, StatsErr
         // static ref
     }
 
-    for (line_number, line) in BufReader::new(&*body).lines().filter_map(|line| line.ok()).enumerate() {
+    for (line_number, line) in BufReader::new(&*body)
+        .lines()
+        .filter_map(|line| line.ok())
+        .enumerate()
+    {
         let clean_line_intermediate = CLEAN_RE.replace(&line, r"\1");
         let clean_line = CLEAN_COMMENTS_RE.replace(&clean_line_intermediate, "");
         if clean_line.starts_with("LEXICON") {
@@ -43,70 +123,13 @@ pub fn get_stats(body: hyper::Chunk) -> Result<Vec<(StatKind, String)>, StatsErr
             })?;
             current_lexicon = Some(lexicon_name.to_string());
         } else if !clean_line.is_empty() && current_lexicon.is_some() {
-            let line_error = format!("Unable to parse L{}", line_number);
-            let token_count = clean_line.split_whitespace().count();
-
-            if token_count >= 2 {
-                if clean_line.contains(':') {
-                    // TODO: me
-                } else {
-                    let mut split = clean_line
-                        .split(';')
-                        .next()
-                        .ok_or_else(|| StatsError::Lexc(line_error.clone()))?
-                        .trim()
-                        .split_whitespace();
-                    let lemma = split
-                        .next()
-                        .ok_or_else(|| StatsError::Lexc(line_error.clone()))?;
-                    let continuation_lexicon = split
-                        .next()
-                        .ok_or_else(|| StatsError::Lexc(line_error.clone()))?
-                        .trim()
-                        .split('-')
-                        .map(|x| x.to_string())
-                        .collect::<BTreeSet<_>>();
-                    // let gloss = if clean_line.contains('!') {
-                    //     Some(clean_line.split('!').nth(1))
-                    // } else {
-                    //     None
-                    // };
-
-                    match lexicons.entry(current_lexicon.as_ref().unwrap().to_string()) {
-                        Entry::Occupied(mut occupied) => {
-                            occupied
-                                .get_mut()
-                                .1
-                                .insert((lemma.to_string(), continuation_lexicon));
-                        }
-                        Entry::Vacant(vacant) => {
-                            vacant.insert((
-                                vec![],
-                                HashSet::from_iter(vec![(lemma.to_string(), continuation_lexicon)]),
-                            ));
-                        }
-                    };
-                }
-            } else if token_count == 1 {
-                let lexicon_pointer = clean_line
-                    .split(';')
-                    .next()
-                    .ok_or_else(|| StatsError::Lexc(line_error.clone()))?
-                    .trim();
-                if lexicon_pointer.contains(' ') {
-                    // TODO: log error
-                } else {
-                    match lexicons.entry(current_lexicon.as_ref().unwrap().to_string()) {
-                        Entry::Occupied(mut occupied) => {
-                            occupied.get_mut().0.push(lexicon_pointer.to_string());
-                        }
-                        Entry::Vacant(vacant) => {
-                            vacant.insert((vec![lexicon_pointer.to_string()], HashSet::new()));
-                        }
-                    };
-                }
-            } else {
-                // TODO: log parse failure
+            if let Err(err) = parse_line(
+                &clean_line,
+                line_number,
+                current_lexicon.as_ref().unwrap(),
+                &mut lexicons,
+            ) {
+                println!("{:?}", err); // TODO: log
             }
         }
     }
@@ -115,7 +138,7 @@ pub fn get_stats(body: hyper::Chunk) -> Result<Vec<(StatKind, String)>, StatsErr
         let reachable_lexicons = get_all_lexicons(&lexicons, "Root");
         let entries = reachable_lexicons
             .iter()
-            .flat_map(|lexicon| lexicons.get(lexicon).unwrap().clone().1)
+            .flat_map(|lexicon| lexicons[lexicon].clone().1)
             .collect::<HashSet<_>>();
         Ok(vec![(StatKind::Stems, entries.len().to_string())])
     } else {
