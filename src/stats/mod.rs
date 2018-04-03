18b38ab384684a20902f8e901c8dd4717fec2738
Sushain Cherivirala <sushain@skc.name>
2018-04-03 13:44:41 -0500

    Modularization of XML stats

--- src/stats/mod.rs
+++ src/stats/mod.rs
@@ -1,9 +1,9 @@
 mod lexc;
+mod xml;
 
 extern crate futures;
 extern crate hyper;
 extern crate hyper_tls;
-extern crate quick_xml;
 extern crate tempfile;
 extern crate tokio_core;
 
@@ -17,8 +17,6 @@ use self::hyper::Client;
 use self::tokio_core::reactor::Core;
 use self::hyper_tls::HttpsConnector;
 use self::tempfile::NamedTempFile;
-use self::quick_xml::reader::Reader;
-use self::quick_xml::events::Event;
 
 use super::models::{FileKind, StatKind};
 
@@ -60,119 +58,13 @@ pub fn get_file_stats(
                 .map_err(StatsError::Hyper)
                 .and_then(move |body| match *file_kind {
                     FileKind::Monodix | FileKind::MetaMonodix => {
-                        let mut reader =
-                            Reader::from_str(str::from_utf8(&*body).map_err(StatsError::Utf8)?);
-                        let mut buf = Vec::new();
-
-                        let mut e_count = 0;
-                        let mut pardef_count = 0;
-                        let mut in_section = false;
-                        let mut in_pardefs = false;
-
-                        loop {
-                            match reader.read_event(&mut buf) {
-                                Ok(Event::Start(ref e)) if e.name() == b"section" => {
-                                    in_section = true
-                                }
-                                Ok(Event::Start(ref e)) if e.name() == b"pardefs" => {
-                                    in_pardefs = true
-                                }
-                                Ok(Event::Start(ref e)) if in_section && e.name() == b"e" => {
-                                    e_count += 1
-                                }
-                                Ok(Event::Start(ref e)) if in_pardefs && e.name() == b"pardef" => {
-                                    pardef_count += 1
-                                }
-                                Ok(Event::End(ref e)) if e.name() == b"section" => {
-                                    in_section = false
-                                }
-                                Ok(Event::End(ref e)) if e.name() == b"pardefs" => {
-                                    in_pardefs = false
-                                }
-                                Ok(Event::Eof) => break,
-                                Err(e) => {
-                                    return Err(StatsError::Xml(format!(
-                                        "Error at position {} in {}: {:?}",
-                                        reader.buffer_position(),
-                                        file_path,
-                                        e
-                                    )));
-                                }
-                                _ => (),
-                            }
-                            buf.clear();
-                        }
-
-                        Ok(vec![
-                            (StatKind::Entries, e_count.to_string()),
-                            (StatKind::Paradigms, pardef_count.to_string()),
-                        ])
+                        self::xml::get_monodix_stats(body, file_path)
                     }
                     FileKind::Bidix | FileKind::MetaBidix | FileKind::Postdix => {
-                        let mut reader =
-                            Reader::from_str(str::from_utf8(&*body).map_err(StatsError::Utf8)?);
-                        let mut buf = Vec::new();
-
-                        let mut e_count = 0;
-                        let mut in_section = false;
-
-                        loop {
-                            match reader.read_event(&mut buf) {
-                                Ok(Event::Start(ref e)) if e.name() == b"section" => {
-                                    in_section = true
-                                }
-                                Ok(Event::Start(ref e)) if in_section && e.name() == b"e" => {
-                                    e_count += 1
-                                }
-                                Ok(Event::End(ref e)) if e.name() == b"section" => {
-                                    in_section = false
-                                }
-                                Ok(Event::Eof) => break,
-                                Err(e) => {
-                                    return Err(StatsError::Xml(format!(
-                                        "Error at position {} in {}: {:?}",
-                                        reader.buffer_position(),
-                                        file_path,
-                                        e
-                                    )));
-                                }
-                                _ => (),
-                            }
-                            buf.clear();
-                        }
-
-                        Ok(vec![(StatKind::Entries, e_count.to_string())])
+                        self::xml::get_bidix_stats(body, file_path)
                     }
                     FileKind::Transfer => {
-                        let mut reader =
-                            Reader::from_str(str::from_utf8(&*body).map_err(StatsError::Utf8)?);
-                        let mut buf = Vec::new();
-
-                        let mut rule_count = 0;
-                        let mut macro_count = 0;
-
-                        loop {
-                            match reader.read_event(&mut buf) {
-                                Ok(Event::Start(ref e)) if e.name() == b"rule" => rule_count += 1,
-                                Ok(Event::Start(ref e)) if e.name() == b"macro" => macro_count += 1,
-                                Ok(Event::Eof) => break,
-                                Err(e) => {
-                                    return Err(StatsError::Xml(format!(
-                                        "Error at position {} in {}: {:?}",
-                                        reader.buffer_position(),
-                                        file_path,
-                                        e
-                                    )));
-                                }
-                                _ => (),
-                            }
-                            buf.clear();
-                        }
-
-                        Ok(vec![
-                            (StatKind::Rules, rule_count.to_string()),
-                            (StatKind::Macros, macro_count.to_string()),
-                        ])
+                        self::xml::get_transfer_stats(body, file_path)
                     }
                     FileKind::Rlx => {
                         let mut rlx_file = NamedTempFile::new().map_err(StatsError::Io)?;
---
+++ src/stats/xml.rs
@@ -0,0 +1,117 @@
+extern crate hyper;
+extern crate quick_xml;
+
+use std::str;
+
+use self::quick_xml::reader::Reader;
+use self::quick_xml::events::Event;
+
+use super::StatsError;
+
+use models::StatKind;
+
+pub fn get_bidix_stats(
+    body: hyper::Chunk,
+    file_path: &str,
+) -> Result<Vec<(StatKind, String)>, StatsError> {
+    let mut reader = Reader::from_str(str::from_utf8(&*body).map_err(StatsError::Utf8)?);
+    let mut buf = Vec::new();
+
+    let mut e_count = 0;
+    let mut in_section = false;
+
+    loop {
+        match reader.read_event(&mut buf) {
+            Ok(Event::Start(ref e)) if e.name() == b"section" => in_section = true,
+            Ok(Event::Start(ref e)) if in_section && e.name() == b"e" => e_count += 1,
+            Ok(Event::End(ref e)) if e.name() == b"section" => in_section = false,
+            Ok(Event::Eof) => break,
+            Err(e) => {
+                return Err(StatsError::Xml(format!(
+                    "Error at position {} in {}: {:?}",
+                    reader.buffer_position(),
+                    file_path,
+                    e
+                )));
+            }
+            _ => (),
+        }
+        buf.clear();
+    }
+
+    Ok(vec![(StatKind::Entries, e_count.to_string())])
+}
+
+pub fn get_monodix_stats(
+    body: hyper::Chunk,
+    file_path: &str,
+) -> Result<Vec<(StatKind, String)>, StatsError> {
+    let mut reader = Reader::from_str(str::from_utf8(&*body).map_err(StatsError::Utf8)?);
+    let mut buf = Vec::new();
+
+    let mut e_count = 0;
+    let mut pardef_count = 0;
+    let mut in_section = false;
+    let mut in_pardefs = false;
+
+    loop {
+        match reader.read_event(&mut buf) {
+            Ok(Event::Start(ref e)) if e.name() == b"section" => in_section = true,
+            Ok(Event::Start(ref e)) if e.name() == b"pardefs" => in_pardefs = true,
+            Ok(Event::Start(ref e)) if in_section && e.name() == b"e" => e_count += 1,
+            Ok(Event::Start(ref e)) if in_pardefs && e.name() == b"pardef" => pardef_count += 1,
+            Ok(Event::End(ref e)) if e.name() == b"section" => in_section = false,
+            Ok(Event::End(ref e)) if e.name() == b"pardefs" => in_pardefs = false,
+            Ok(Event::Eof) => break,
+            Err(e) => {
+                return Err(StatsError::Xml(format!(
+                    "Error at position {} in {}: {:?}",
+                    reader.buffer_position(),
+                    file_path,
+                    e
+                )));
+            }
+            _ => (),
+        }
+        buf.clear();
+    }
+
+    Ok(vec![
+        (StatKind::Entries, e_count.to_string()),
+        (StatKind::Paradigms, pardef_count.to_string()),
+    ])
+}
+
+pub fn get_transfer_stats(
+    body: hyper::Chunk,
+    file_path: &str,
+) -> Result<Vec<(StatKind, String)>, StatsError> {
+    let mut reader = Reader::from_str(str::from_utf8(&*body).map_err(StatsError::Utf8)?);
+    let mut buf = Vec::new();
+
+    let mut rule_count = 0;
+    let mut macro_count = 0;
+
+    loop {
+        match reader.read_event(&mut buf) {
+            Ok(Event::Start(ref e)) if e.name() == b"rule" => rule_count += 1,
+            Ok(Event::Start(ref e)) if e.name() == b"macro" => macro_count += 1,
+            Ok(Event::Eof) => break,
+            Err(e) => {
+                return Err(StatsError::Xml(format!(
+                    "Error at position {} in {}: {:?}",
+                    reader.buffer_position(),
+                    file_path,
+                    e
+                )));
+            }
+            _ => (),
+        }
+        buf.clear();
+    }
+
+    Ok(vec![
+        (StatKind::Rules, rule_count.to_string()),
+        (StatKind::Macros, macro_count.to_string()),
+    ])
+}
