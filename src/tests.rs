bf940fe3668603e1d38badd725d18eee56da715f
Sushain Cherivirala <sushain@skc.name>
2018-04-16 18:25:26 -0500

    Add real test

--- src/tests.rs
+++ src/tests.rs
@@ -1,4 +1,6 @@
 use std::process::{Command, Stdio};
+use std::time::Duration;
+use std::thread::sleep;
 
 extern crate serde_json;
 extern crate tempfile;
@@ -8,6 +10,9 @@ use rocket::http::Status;
 use self::tempfile::NamedTempFile;
 
 use super::rocket;
+
+const INITIAL_WAIT_DURATION: u64 = 1;
+const MAX_WAIT_DURATION: u64 = 32;
 
 macro_rules! run_test {
     (|$client:ident| $block:expr) => ({
@@ -46,8 +51,8 @@ fn test_nonexistent_package_stats() {
         assert_eq!(response.status(), Status::BadRequest);
         let body = parse_response(response);
         assert_eq!(body["name"], "apertium-xxx");
-        let error = body["error"].to_string();
-        assert!(error.starts_with("\"Package not found"), error);
+        let error = body["error"].as_str().expect("error is string");
+        assert!(error.starts_with("Package not found"), error.to_string());
     });
 }
 
@@ -60,9 +65,74 @@ fn test_invalid_package_stats() {
         assert_eq!(
             body,
             json!({
-            "error": "Invalid package name: abcd",
-            "name": "abcd"
-        })
+                "error": "Invalid package name: abcd",
+                "name": "abcd"
+            })
         );
+    });
+}
+
+#[test]
+fn test_module_stats() {
+    run_test!(|client| {
+        let response = client.get("/apertium-cat").dispatch();
+        assert_eq!(response.status(), Status::Accepted);
+        let mut body = parse_response(response);
+        assert_eq!(body["name"], "apertium-cat");
+
+        let in_progress = body["in_progress"]
+            .as_array_mut()
+            .expect("valid in_progress");
+        assert_eq!(in_progress.len(), 2);
+        in_progress
+            .sort_by_key(|entry| entry["kind"].as_str().expect("kind is string").to_string());
+        assert_eq!(in_progress[0]["kind"], "Monodix");
+        assert_eq!(in_progress[0]["path"], "apertium-cat.cat.dix");
+        let revision = in_progress[0]["revision"]
+            .as_i64()
+            .expect("revision is i64");
+        assert!(revision > 500, revision);
+
+        let mut sleep_duration = Duration::from_secs(INITIAL_WAIT_DURATION);
+        while sleep_duration < Duration::from_secs(MAX_WAIT_DURATION) {
+            let response = client.get("/apertium-cat").dispatch();
+            match response.status() {
+                Status::TooManyRequests => {
+                    println!("Waiting for OK... ({:?})", sleep_duration);
+                }
+                Status::Ok => {
+                    let mut body = parse_response(response);
+                    if body["in_progress"]
+                        .as_array()
+                        .expect("valid in_progress")
+                        .is_empty()
+                    {
+                        assert_eq!(body["name"], "apertium-cat");
+                        let mut stats = body["stats"].as_array_mut().expect("valid stats");
+                        assert_eq!(stats.len(), 3);
+                        stats.sort_by_key(|entry| {
+                            entry["path"].as_str().expect("path is string").to_string()
+                        });
+                        assert_eq!(stats[0]["file_kind"], "Monodix");
+                        assert_eq!(stats[0]["stat_kind"], "Entries");
+                        assert_eq!(stats[0]["path"], "apertium-cat.cat.dix");
+                        assert!(stats[0]["revision"].as_i64().expect("revision is i64") > 500);
+                        let value = stats[0]["value"]
+                            .as_str()
+                            .expect("value is string")
+                            .parse::<i32>()
+                            .expect("value is i32");
+                        assert!(value > 50000, value);
+                        return;
+                    }
+                }
+                _ => assert!(false, "recieved invalid status"),
+            }
+
+            sleep(sleep_duration);
+            sleep_duration *= 2;
+        }
+
+        assert!(false, "failed to fetch statistics before timeout");
     });
 }
