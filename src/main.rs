6108a96cd60a1389ce0680e03045df7b73e8a58e
Sushain Cherivirala <sushain@skc.name>
2018-04-16 13:21:45 -0500

    More refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-#![feature(plugin, custom_attribute, try_trait, custom_derive, conservative_impl_trait,
-           expanded_impl_trait)]
+#![feature(plugin, custom_attribute, try_trait, custom_derive, conservative_impl_trait)]
 #![plugin(rocket_codegen)]
 #![deny(clippy)]
 #![allow(needless_pass_by_value)]
