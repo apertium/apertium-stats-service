85c91c4805d888aac2f7d1a9cc2d2892ecfe4e0f
Sushain Cherivirala <sushain@skc.name>
2018-04-16 14:19:28 -0500

    cargo fmt

--- src/tests.rs
+++ src/tests.rs
@@ -11,5 +11,10 @@ fn test_usage() {
     let client = Client::new(rocket()).expect("valid rocket instance");
     let mut response = client.get("/").dispatch();
     assert_eq!(response.status(), Status::Ok);
-    assert!(response.body_string().expect("non-empty body").starts_with("USAGE"));
+    assert!(
+        response
+            .body_string()
+            .expect("non-empty body")
+            .starts_with("USAGE")
+    );
 }
