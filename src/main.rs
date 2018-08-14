844179fdea7c501ce76010d5a53b24cd6a13c667
Sushain Cherivirala <sushain@skc.name>
2018-08-13 23:42:52 -0700

    Update some versions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![feature(plugin, custom_attribute, try_trait, custom_derive, inclusive_range_methods)]
+#![feature(plugin, custom_attribute, try_trait, custom_derive)]
 #![plugin(rocket_codegen)]
 #![deny(clippy)]
 #![allow(needless_pass_by_value)]
