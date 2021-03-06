77f09376ef51e6fcbc19fe526e2a49a65f3e353d
Sushain Cherivirala <sushain@skc.name>
2021-03-05 22:42:44 -0800

    Fix annotation

--- src/main.rs
+++ src/main.rs
@@ -522,7 +522,7 @@ pub fn service(
     rocket(pool, worker, logger, package_listing_routes_enabled)
 }
 
-#[cfg_attr(tarpaulin, skip)]
+#[cfg(not(tarpaulin_include))]
 fn main() {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
