dd5eb87998b5e6551606df38e3361563cb8a561f
Daniel Swanson <popcorn.tomato.dude@gmail.com>
2021-03-12 21:28:10 -0600

    run cargo fmt

--- build.rs
+++ build.rs
@@ -2,9 +2,11 @@ use cc;
 use std::path::PathBuf;
 
 fn main() {
-    let ts_dir: PathBuf = ["src", "stats", "tree-sitter-apertium", "tree-sitter-lexd", "src"].iter().collect();
-	cc::Build::new()
-		.include(&ts_dir)
-		.file(ts_dir.join("parser.c"))
-		.compile("tree-sitter-lexd");
+    let ts_dir: PathBuf = ["src", "stats", "tree-sitter-apertium", "tree-sitter-lexd", "src"]
+        .iter()
+        .collect();
+    cc::Build::new()
+        .include(&ts_dir)
+        .file(ts_dir.join("parser.c"))
+        .compile("tree-sitter-lexd");
 }
--- src/stats/lexd.rs
+++ src/stats/lexd.rs
@@ -1,8 +1,6 @@
-use std::{
-    collections::{HashSet}
-};
+use std::collections::HashSet;
 
-use tree_sitter::{Parser, Language, TreeCursor};
+use tree_sitter::{Language, Parser, TreeCursor};
 
 use rocket_contrib::{json, json::JsonValue};
 
@@ -10,7 +8,9 @@ use slog::{warn, Logger};
 
 use crate::{models::StatKind, stats::StatsError};
 
-extern "C" { fn tree_sitter_lexd() -> Language; }
+extern "C" {
+    fn tree_sitter_lexd() -> Language;
+}
 
 pub fn get_stats(logger: &Logger, body: &str) -> Result<Vec<(StatKind, JsonValue)>, StatsError> {
     let mut parser = Parser::new();
@@ -18,40 +18,40 @@ pub fn get_stats(logger: &Logger, body: &str) -> Result<Vec<(StatKind, JsonValue
     parser.set_language(language).unwrap();
     let tree = parser.parse(body, None).unwrap();
     let mut lexicons: HashSet<String> = HashSet::new();
-	let mut patterns: HashSet<String> = HashSet::new();
-	let mut lex_entries: usize = 0;
-	let mut pat_entries: usize = 0;
+    let mut patterns: HashSet<String> = HashSet::new();
+    let mut lex_entries: usize = 0;
+    let mut pat_entries: usize = 0;
 
-	let mut walker: TreeCursor = tree.root_node().walk();
+    let mut walker: TreeCursor = tree.root_node().walk();
 
     for child in tree.root_node().children(&mut walker) {
-		let mut walker2: TreeCursor = child.walk();
-		if child.kind() == "pattern_block" {
-		   if child.child(0).unwrap().kind() == "pattern_start" {
-			  patterns.insert("".to_string());
-		   }
-		   for line in child.children(&mut walker2) {
-		   	   if line.kind() == "pattern_line" {
-				   pat_entries += 1;
-			   } else if line.kind() == "identifier" {
-			   	  patterns.insert(body[line.byte_range()].to_string());
-			   }
-		   }
-		} else if child.kind() == "lexicon_block" {
-		   for line in child.children(&mut walker2) {
-		   	   if line.kind() == "lexicon_line" {
-			   	  lex_entries += 1;
-			   } else if line.kind() == "identifier" {
-			   	  lexicons.insert(body[line.byte_range()].to_string());
-			   }
-		   }
-		}
-	}
+        let mut walker2: TreeCursor = child.walk();
+        if child.kind() == "pattern_block" {
+            if child.child(0).unwrap().kind() == "pattern_start" {
+                patterns.insert("".to_string());
+            }
+            for line in child.children(&mut walker2) {
+                if line.kind() == "pattern_line" {
+                    pat_entries += 1;
+                } else if line.kind() == "identifier" {
+                    patterns.insert(body[line.byte_range()].to_string());
+                }
+            }
+        } else if child.kind() == "lexicon_block" {
+            for line in child.children(&mut walker2) {
+                if line.kind() == "lexicon_line" {
+                    lex_entries += 1;
+                } else if line.kind() == "identifier" {
+                    lexicons.insert(body[line.byte_range()].to_string());
+                }
+            }
+        }
+    }
 
-	Ok(vec![
-		(StatKind::Lexicons, json!(lexicons.len())),
-		(StatKind::LexiconEntries, json!(lex_entries)),
-		(StatKind::Patterns, json!(patterns.len())),
-		(StatKind::PatternEntries, json!(pat_entries)),
-	])
+    Ok(vec![
+        (StatKind::Lexicons, json!(lexicons.len())),
+        (StatKind::LexiconEntries, json!(lex_entries)),
+        (StatKind::Patterns, json!(patterns.len())),
+        (StatKind::PatternEntries, json!(pat_entries)),
+    ])
 }
