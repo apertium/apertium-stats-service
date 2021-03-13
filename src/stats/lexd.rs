8d52cb6b337f94101554c771dd73e0350275aa5f
Daniel Swanson <popcorn.tomato.dude@gmail.com>
2021-03-13 10:58:18 -0600

    checkout submodules, don't .unwrap(), borrow strings

--- src/stats/lexd.rs
+++ src/stats/lexd.rs
@@ -1,8 +1,8 @@
 use std::collections::HashSet;
 
-use tree_sitter::{Language, Parser, TreeCursor};
 use rocket_contrib::{json, json::JsonValue};
 use slog::Logger;
+use tree_sitter::{Language, Parser, TreeCursor};
 
 use crate::{models::StatKind, stats::StatsError};
 
@@ -13,10 +13,14 @@ extern "C" {
 pub fn get_stats(_logger: &Logger, body: &str) -> Result<Vec<(StatKind, JsonValue)>, StatsError> {
     let mut parser = Parser::new();
     let language = unsafe { tree_sitter_lexd() };
-    parser.set_language(language).unwrap();
-    let tree = parser.parse(body, None).unwrap();
-    let mut lexicons: HashSet<String> = HashSet::new();
-    let mut patterns: HashSet<String> = HashSet::new();
+    parser
+        .set_language(language)
+        .map_err(|e| StatsError::Lexd(format!("Unable to load tree-sitter parser: {}", e)))?;
+    let tree = parser
+        .parse(body, None)
+        .ok_or_else(|| StatsError::Lexd(format!("Unable to parse lexd file")))?;
+    let mut lexicons: HashSet<&str> = HashSet::new();
+    let mut patterns: HashSet<&str> = HashSet::new();
     let mut lex_entries: usize = 0;
     let mut pat_entries: usize = 0;
 
@@ -26,13 +30,13 @@ pub fn get_stats(_logger: &Logger, body: &str) -> Result<Vec<(StatKind, JsonValu
         let mut walker2: TreeCursor = child.walk();
         if child.kind() == "pattern_block" {
             if child.child(0).unwrap().kind() == "pattern_start" {
-                patterns.insert("".to_string());
+                patterns.insert("");
             }
             for line in child.children(&mut walker2) {
                 if line.kind() == "pattern_line" {
                     pat_entries += 1;
                 } else if line.kind() == "identifier" {
-                    patterns.insert(body[line.byte_range()].to_string());
+                    patterns.insert(&body[line.byte_range()]);
                 }
             }
         } else if child.kind() == "lexicon_block" {
@@ -40,7 +44,7 @@ pub fn get_stats(_logger: &Logger, body: &str) -> Result<Vec<(StatKind, JsonValu
                 if line.kind() == "lexicon_line" {
                     lex_entries += 1;
                 } else if line.kind() == "identifier" {
-                    lexicons.insert(body[line.byte_range()].to_string());
+                    lexicons.insert(&body[line.byte_range()]);
                 }
             }
         }
