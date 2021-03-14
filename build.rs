2baac4d2c701f3c52493a2c2d74802299e514dda
Sushain Cherivirala <sushain@skc.name>
2021-03-13 20:54:02 -0800

    Single cc build

--- build.rs
+++ build.rs
@@ -1,17 +1,10 @@
 use std::path::PathBuf;
 
 fn main() {
-    // tree-sitter-lexd
     let ts_lexd_dir = PathBuf::from(r"src/stats/tree-sitter-apertium/tree-sitter-lexd/src");
-    cc::Build::new()
-        .include(&ts_lexd_dir)
-        .file(ts_lexd_dir.join("parser.c"))
-        .compile("tree-sitter-lexd");
-
-    // tree-sitter-cg
     let ts_cg_dir = PathBuf::from(r"src/stats/tree-sitter-apertium/tree-sitter-cg/src");
     cc::Build::new()
-        .include(&ts_cg_dir)
-        .file(ts_cg_dir.join("parser.c"))
-        .compile("tree-sitter-cg");
+        .includes(vec![&ts_lexd_dir, &ts_cg_dir])
+        .files(vec![ts_lexd_dir.join("parser.c"), ts_cg_dir.join("parser.c")])
+        .compile("tree-sitter");
 }
